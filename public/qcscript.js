8fdd5d4365feaa66a69988a71c07a419d73de3d8
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-02-03 14:06:10 +0700

    Update qcscript.js

--- public/qcscript.js
+++ public/qcscript.js
@@ -1,6 +1,30 @@
 // check width
 window.addEventListener("load", function () {
     console.log("xxxsss", window.innerWidth)
+
+    var qcImgDiv = document.getElementById('qcImg');
+
+if (qcImgDiv) {
+    var scriptElement = document.createElement('script');
+    scriptElement.async = true;
+    scriptElement.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3619133031508264';
+    scriptElement.crossOrigin = 'anonymous';
+
+    // Chèn đối tượng script vào thẻ div
+    qcImgDiv.appendChild(scriptElement);
+    // Tạo đối tượng ins element
+    var insElement = document.createElement('ins');
+    insElement.className = 'adsbygoogle';
+    insElement.style.display = 'block';
+    insElement.setAttribute('data-ad-client', 'ca-pub-3619133031508264');
+    insElement.setAttribute('data-ad-slot', '9137554578');
+    insElement.setAttribute('data-ad-format', 'auto');
+    insElement.setAttribute('data-full-width-responsive', 'true');
+
+    // Chèn đối tượng ins vào thẻ div
+    qcImgDiv.appendChild(insElement);
+}
+    
     if (window.innerWidth <= 500) {
 
         // Giua bai mgid
